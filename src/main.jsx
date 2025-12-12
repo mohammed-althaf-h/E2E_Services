b73f1bc2b7f190150a209a1f28df5254d32ff853
Mohammed Althaf H <50052789+mohammed-althaf-h@users.noreply.github.com>
2025-12-12 19:09:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '@/App';
 import '@/index.css';
 
+// If you created the ThemeProvider file:
+import { ThemeProvider } from '@/providers/ThemeProvider';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
